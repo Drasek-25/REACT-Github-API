2a1fa3e8cce8e9c0de19947c42331d77a4537062
Patrick Marschlowitz <p.marschlowitz@gmail.com>
2020-06-09 15:53:07 -0700

    fixed warnings in browser, including imae key props, and removing import link from repos

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,35 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Repos = (props) => {
+   const [repos, setrepos] = useState(null);
 
-    const [repos, setrepos] = useState(null)
+   useEffect(() => {
+      const fetchData = async () => {
+         const result = await axios(`${props.user.url}/repos`);
+         setrepos(result.data);
+      };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await axios(
-                `${props.user.url}/repos`
-            );
-            setrepos(result.data);
-        };
-
-        fetchData();
-    }, [props.user]);
-    console.log(repos)
-    return (
-        <div className="followers-page">
-            <h3>{props.user.login}'s repositories</h3>
-            {!repos
-                ? <h3>LOADING....</h3>
-                : repos.map((repo, id) => (
-                    <div key={id} className='follower row'>
-                        <a href={`https://github.com/${repo.owner.login}/${repo.name}`}>{repo.name}</a>
-                    </div>
-
-                ))}
-        </div>
-    )
-}
+      fetchData();
+   }, [props.user]);
+   console.log(repos);
+   return (
+      <div className="followers-page">
+         <h3>{props.user.login}'s repositories</h3>
+         {!repos ? (
+            <h3>LOADING....</h3>
+         ) : (
+            repos.map((repo, id) => (
+               <div key={id} className="follower row">
+                  <a
+                     href={`https://github.com/${repo.owner.login}/${repo.name}`}
+                  >
+                     {repo.name}
+                  </a>
+               </div>
+            ))
+         )}
+      </div>
+   );
+};
 
 export default Repos;
